219fd7ca846541c202e4c16423037c8e071dd6fe
Ilya Grishanov <grishanov@me.com>
2024-12-22 03:39:17 +0300

    Image, DateLabel and Like Button added

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Ilya Grishanov on 21.12.2024.
+//
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+}
