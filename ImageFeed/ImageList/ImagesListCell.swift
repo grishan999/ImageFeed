d452ca1df2f07bfa6be787e3c26fab7a0e8f58db
Ilya Grishanov <grishanov@me.com>
2024-12-22 15:19:45 +0300

    New cells added

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -9,4 +9,8 @@ import UIKit
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
     
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    
 }
