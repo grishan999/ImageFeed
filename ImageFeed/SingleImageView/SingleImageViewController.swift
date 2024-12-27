680481097bc30e4cc99ac0af120996491cff223a
Ilya Grishanov <grishanov@me.com>
2024-12-27 18:28:15 +0300

    Present View Controller added

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -15,6 +15,12 @@ final class SingleImageViewController: UIViewController {
            }
        }
     
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
