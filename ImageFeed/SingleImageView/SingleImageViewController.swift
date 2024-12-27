10d5d80c35ec3835c7a99e60175fd2aea1c77882
Ilya Grishanov <grishanov@me.com>
2024-12-27 16:24:37 +0300

    Single Image View added

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Ilya Grishanov on 27.12.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
