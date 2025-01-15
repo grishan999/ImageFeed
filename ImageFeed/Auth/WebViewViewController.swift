d73655aab9de264c552aba6882b3a57b56ef42dc
Ilya Grishanov <grishanov@me.com>
2025-01-16 00:59:00 +0300

    webView added

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Ilya Grishanov on 16.01.2025.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    
+    
+    @IBOutlet private var webView: WKWebView!
+    
+}
