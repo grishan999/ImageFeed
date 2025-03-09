d43fd803e449e3d7bfa1879a89ec791cf81b7a62
Ilya Grishanov <grishanov@me.com>
2025-03-09 20:54:59 +0300

    ProfileViewController refactored, ProfilePresenterAdded

---
+++ ImageFeedTests/WebView/WebViewPresenterSpy.swift
@@ -0,0 +1,27 @@
+//
+//  WebViewPresenterSpy.swift
+//  ImageFeed
+//
+//  Created by Ilya Grishanov on 07.03.2025.
+//
+
+import ImageFeed
+import Foundation
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+
+    func didUpdateProgressValue(_ newValue: Double) {
+
+    }
+
+    func handleAuthorizationCode(from url: URL) -> String? {
+        return nil
+    }
+}
+
